896f3cb000dab3c0749daafd8318e791e55fcea1
SCHAFF <julien.schaffauser@epita.fr>
2024-10-23 17:20:47 +0200

    add dialoLLM model

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-import joblib
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from fastapi import FastAPI
+import torch
 app = FastAPI()
 
-model = joblib.load("regression.joblib")
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
 
-@app.post("/predict")
+@app.post("/chat")
 def predict(data: dict):
-    size = data['size']
-    nb_rooms = data['nb_rooms']
-    garden = data['garden']
-    price = model.predict([[size, nb_rooms, garden]])[0]
-    return {"price": price}
+    input_text = data["input"]
+    new_user_input_ids = tokenizer.encode(input_text + tokenizer.eos_token, return_tensors='pt')
+    response = model.generate(new_user_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    return {"response": tokenizer.decode(response[:, new_user_input_ids.shape[-1]:][0], skip_special_tokens=True)}
